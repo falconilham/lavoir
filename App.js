1dbd263aabdc47cdacb3a2f87584983f3c0927ab
ilhamheryanto <mohamad.heryanto@gdis-world.com>
2020-02-18 14:17:48 +0700

    First Commit

--- App.js
+++ App.js
@@ -8,8 +8,8 @@
  * @flow
  */
 import React, { useState } from 'react';
-import { TextInput, Text, View, StyleSheet } from 'react-native';
-import { DatePicker, Button, Provider } from '@ant-design/react-native';
+import { TextInput, Text, View, StyleSheet, ScrollView, Button } from 'react-native';
+import { DatePicker, Provider } from '@ant-design/react-native';
 
 function App() {
   let [data, setData] = useState({
@@ -25,93 +25,103 @@ function App() {
   console.log(data);
   return (
     <Provider>
-      <View style={styles.container}>
-        <Text styles={styles.fontLogo}>Lavoir Wedding</Text>
-        <View style={styles.form}>
-          <Text styles={styles.font}>Form</Text>
-          <View style={styles.boxInput}>
-            <Text style={styles.font}>Pria</Text>
-            <TextInput
-              placeholder="Nama Mempelai Pria"
-              style={styles.inputText}
-              value={data.nama.pria}
-              onChangeText={(value) => setData({
-                ...data,
-                nama: {
-                  pria: value,
-                  wanita: data.nama.wanita,
-                },
-              })}
-            />
-          </View>
-          <View style={styles.boxInput}>
-            <Text style={styles.font}>Wanita</Text>
-            <TextInput
-              placeholder="Nama Mempelai Wanita"
-              style={styles.inputText}
-              value={data.nama.wanita}
-              onChangeText={(value) => setData({
-                ...data,
-                nama: {
-                  wanita: value,
-                  pria: data.nama.pria,
-                },
-              })}
-            />
-          </View>
-          <View style={styles.boxInput}>
-            <Text style={styles.font}>Tanggal</Text>
-            <View style={{ flexDirection: 'row' }}>
+      <ScrollView style={styles.container}>
+        <View style={styles.containerWrapper}>
+          <Text style={styles.fontLogo}>Lavoir Wedding</Text>
+          <Text style={styles.font}>Form Registrasi Pengantin</Text>
+          <View style={styles.form}>
+            <View style={styles.boxInput}>
+              <Text style={styles.font}>Pria</Text>
               <TextInput
-                style={styles.datePlaceholder}
-                editable={false}
-                value={date.getFullYear().toString()}
+                placeholder="Nama Mempelai Pria"
+                style={styles.inputText}
+                value={data.nama.pria}
+                onChangeText={(value) => setData({
+                  ...data,
+                  nama: {
+                    pria: value,
+                    wanita: data.nama.wanita,
+                  },
+                })}
               />
+            </View>
+            <View style={styles.boxInput}>
+              <Text style={styles.font}>Wanita</Text>
               <TextInput
-                editable={false}
-                style={styles.datePlaceholder}
-                value={date.getMonth().toString()}
+                placeholder="Nama Mempelai Wanita"
+                style={styles.inputText}
+                value={data.nama.wanita}
+                onChangeText={(value) => setData({
+                  ...data,
+                  nama: {
+                    wanita: value,
+                    pria: data.nama.pria,
+                  },
+                })}
               />
+            </View>
+            <View style={styles.boxInput}>
+              <Text style={styles.font}>Tanggal</Text>
+              <View style={{ flexDirection: 'row' }}>
+                <TextInput
+                  style={styles.datePlaceholder}
+                  editable={false}
+                  value={date.getFullYear().toString()}
+                />
+                <TextInput
+                  editable={false}
+                  style={styles.datePlaceholder}
+                  value={date.getMonth().toString()}
+                />
+                <TextInput
+                  editable={false}
+                  style={styles.datePlaceholder}
+                  value={date.getDay().toString()}
+                />
+                <DatePicker
+                  value={date}
+                  mode="date"
+                  defaultDate={new Date()}
+                  onChange={(e) => setDate(e)}
+                  format="DD-MM-YYYY"
+                >
+                  <Button title='Pick Date' />
+                </DatePicker>
+              </View>
+            </View>
+            <View style={styles.boxInput}>
+              <Text style={styles.font}>Budget</Text>
               <TextInput
-                editable={false}
-                style={styles.datePlaceholder}
-                value={date.getDay().toString()}
+                style={styles.inputText}
+                value={data.budget}
+                onChangeText={(value) => setData({
+                  ...data,
+                  budget: value,
+                })}
+                keyboardType="numeric"
+                maxLength={20}
               />
-              <DatePicker
-                value={date}
-                mode="date"
-                defaultDate={new Date()}
-                onChange={(e) => setDate(e)}
-                format="DD-MM-YYYY"
-              >
-                <Button>Pick Date</Button>
-              </DatePicker>
+            </View>
+            <View>
+              <Button title='Submit' onPress={console.log(data, date)} />
             </View>
           </View>
-          <View style={styles.boxInput}>
-            <Text style={styles.font}>Budget</Text>
-            <TextInput
-              style={styles.inputText}
-              value={data.budget}
-              onChangeText={(value) => setData({
-                ...data,
-                budget: value,
-              })}
-              keyboardType="numeric"
-              maxLength={20}
-            />
-          </View>
         </View>
-      </View>
+      </ScrollView>
     </Provider>
   );
 }
 let styles = StyleSheet.create({
-  container: {
+  containerWrapper: {
+    height: '100%',
+    backgroundColor: 'black',
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'space-evenly',
+    justifyContent: 'space-around',
+  },
+  container: {
     height: '100%',
+    maxHeight: '100%',
     backgroundColor: 'black',
     borderColor: '#DABB56',
     borderWidth: 1,
@@ -120,6 +130,7 @@ let styles = StyleSheet.create({
     width: '80%',
     marginHorizontal: 10,
     color: 'white',
+    marginBottom: 50,
   },
   inputDate: {
     width: '50%',
@@ -144,9 +155,14 @@ let styles = StyleSheet.create({
   },
   font: {
     color: 'white',
+    textAlign: 'center',
+  },
+  fontLabel: {
+    color: 'white',
   },
   fontLogo: {
     color: '#DABB56',
+    textAlign: 'center',
   },
 });
 
