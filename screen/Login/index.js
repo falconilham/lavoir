c4f049a32f1fd41262cc3b8fea253ee2b9323bf4
ilhamheryanto <mohamad.heryanto@gdis-world.com>
2021-01-15 16:29:31 +0700

    add feat transaction

--- screen/Login/index.js
+++ screen/Login/index.js
@@ -4,10 +4,11 @@ import {
   View,
   TextInput,
   Text,
-  ScrollView,
+  TouchableHighlight,
   TouchableOpacity,
   ActivityIndicator,
-  Image
+  Image,
+  Modal
 } from 'react-native';
 import LogoImage from './../../image/logoLavoir.jpeg';
 import { connect } from 'react-redux'
@@ -15,10 +16,15 @@ import styles from '../utils/css';
 import { Formik } from 'formik';
 import firebase from '../config'
 import { addAccount } from '../../reducers/Account';
+import validationSchema from './validationSchema'
 
 function Login(props) {
   let { addAccount, account } = props
   let [isLoading, setIsLoading] = useState(false);
+  let [error, setError] = useState({
+    isError: false,
+    errorMessage: ""
+  })
   let loginFirebase = async (email, password) => {
     setIsLoading(true);
     await firebase
@@ -29,11 +35,13 @@ function Login(props) {
         setIsLoading(false);
       })
       .catch(error => {
-        console.log(error);
+        setError({
+          isError: true,
+          errorMessage: error?.t?.message
+        })
         setIsLoading(false);
       });
   };
-
   useEffect(() => {
     async function isLogin() {
       setIsLoading(true)
@@ -56,77 +64,93 @@ function Login(props) {
     }
     isLogin()
   }, [])
-  if (isLoading) {
-    return (
-      <View style={{alignItems: 'center', justifyContent: 'center'}}>
-         <ActivityIndicator/>
-      </View>
-    )
-  } else {
-    return (
-      <View style={styles.body}>
-        <Formik
-          enableReinitialize
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          onSubmit={value => {
-            loginFirebase(value.email, value.password)
-          }}>
-          {props => {
-            let { values, setFieldValue, handleReset, handleSubmit, dirty } = props;
-            return (
-              <View style={styles.form}>
-                <View style={styles.wrapperImage}>
-                  <Image source={LogoImage} style={styles.logoImage} />
-                </View>
-                <View style={styles.boxInput}>
-                  <Text style={styles.font}>Email</Text>
-                  <View>
-                    <TextInput
-                      placeholder="Email"
-                      placeholderTextColor="#a67c00"
-                      style={styles.inputText}
-                      value={values.email}
-                      onChangeText={value => setFieldValue('email', value)}
-                    />
-                  </View>
-                </View>
-                <View style={styles.boxInput}>
-                  <Text style={styles.font}>Password</Text>
-                  <View>
-                    <TextInput
-                      placeholder="Password"
-                      placeholderTextColor="#a67c00"
-                      style={styles.inputText}
-                      secureTextEntry
-                      value={values.password}
-                      onChangeText={value => setFieldValue('password', value)}
-                    />
-                  </View>
-                </View>
-                <View style={styles.boxButton}>
-                  <TouchableOpacity
-                    onPress={handleReset}
-                    style={styles.buttonReset}>
-                    <Text style={styles.font}>Reset</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity disabled={isLoading} onPress={() => handleSubmit()}>
-                    {isLoading ? (
-                      <ActivityIndicator size="small" color="white" />
-                    ) : (
-                        <Text disabled={!dirty} style={styles.font}>Login</Text>
-                      )}
-                  </TouchableOpacity>
+  return (
+    <View style={styles.body}>
+      <Modal animationType="slide" visible={error.isError} style={{width: '30%', height: '30%'}}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Text co>{error.errorMessage}</Text>
+            </View>
+            <TouchableHighlight onPress={() => setError({isError: false, errorMessage: ''})}>
+              <Text>Close</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+      <Formik
+        enableReinitialize
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={value => {
+          loginFirebase(value.email, value.password)
+        }}>
+        {props => {
+          let { values, setFieldValue, handleReset, handleSubmit, dirty, errors, touched } = props;
+          console.log(errors, touched)
+          return (
+            <View style={styles.form}>
+              <View style={styles.wrapperImage}>
+                <Image source={LogoImage} style={styles.logoImage} />
+              </View>
+              <View style={styles.boxInput}>
+                <Text style={styles.font}>Email</Text>
+                <View>
+                  <TextInput
+                    placeholder="Email"
+                    placeholderTextColor="#a67c00"
+                    style={styles.inputText}
+                    value={values.email}
+                    onChangeText={value => setFieldValue('email', value)}
+                    disabled={isLoading}
+                  />
+                  {errors.email && touched.email && (
+                    <Text style={{color: 'red'}}>{errors.email}</Text>
+                  )}
                 </View>
               </View>
-            );
-          }}
-        </Formik>
-      </View>
-    )
-  }
+              <View style={styles.boxInput}>
+                <Text style={styles.font}>Password</Text>
+                <View>
+                  <TextInput
+                    placeholder="Password"
+                    placeholderTextColor="#a67c00"
+                    style={styles.inputText}
+                    secureTextEntry
+                    value={values.password}
+                    onChangeText={value => setFieldValue('password', value)}
+                    disabled={isLoading}
+                  />
+                  {errors.password && touched.password && (
+                    <Text style={{color: 'red'}}>{errors.password}</Text>
+                  )}
+                </View>
+              </View>
+              <View style={styles.boxButton}>
+                <TouchableOpacity
+                  onPress={handleReset}
+                  style={styles.buttonReset}>
+                  <Text style={styles.font}>Reset</Text>
+                </TouchableOpacity>
+                  {isLoading ? (
+                    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                      <ActivityIndicator size="small" color="white" />
+                    </View>
+                  ) :(
+                    <TouchableOpacity disabled={isLoading || !dirty} onPress={() => handleSubmit()}>
+                      <Text disabled={!dirty} style={!dirty ? {...styles.font, color: 'grey' }: styles.font}>Login</Text>
+                    </TouchableOpacity>
+                  )}
+              </View>
+            </View>
+          );
+        }}
+      </Formik>
+    </View>
+  )
 }
 
 const mapStateToProps = (state) => {
--- screen/utils/css.js
+++ screen/utils/css.js
@@ -109,6 +109,27 @@ let styles = StyleSheet.create({
     height: 100,
     marginVertical: 10,
     padding: 5
+  },
+  centeredView:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+    backgroundColor: "transparent",
+  },
+  modalView:{
+    margin: 20,
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
   }
 });
 
